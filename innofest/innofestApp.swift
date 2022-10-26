1dd6528fbb97ff52ce86e413fab5bf58235b6cd2
harish-f <harish_ram@s2021.ssts.edu.sg>
2022-10-26 12:16:08 +0800

    Initial Commit

---
+++ innofest/innofestApp.swift
@@ -0,0 +1,17 @@
+//
+//  innofestApp.swift
+//  innofest
+//
+//  Created by HARISH RAM BAGHAVATH stu on 26/10/22.
+//
+
+import SwiftUI
+
+@main
+struct innofestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
